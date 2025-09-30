94de25811e55e465826bfa3bac43d74ca761dd4b
Ajay <ajayrajnegi111@gmail.com>
2025-09-30 17:16:19 +0530

    UI cleanup

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BlogController;
 use App\Http\Controllers\Api\AirportController;
 
-//Route::apiResource('blogs', BlogController::class);
-
 Route::get('/blogs',[BlogController::class, 'index']);
 Route::get('/blogs/{slug}', [BlogController::class, 'individualBlog']);
 Route::get('/category', [BlogController::class, 'category']);
 Route::get('/blogs/category/{category}', [BlogController::class, 'byCategory']);
 
-Route::get('/airports',[AirportController::class, 'allAirports']);
+
 Route::get('/country',[AirportController::class, 'country']);
+Route::get('/airports',[AirportController::class, 'allAirports']);
 Route::get('/airport/{country}',[AirportController::class, 'byCountry']);
 Route::get('/airport/country/{id}',[AirportController::class, 'airport']);
